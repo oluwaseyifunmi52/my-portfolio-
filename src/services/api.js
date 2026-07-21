484b6cdd8e4cc89c9d06906686edab30cd8a7525
unknown <oluwaseyifunmioluwadami@email.com>
2026-07-21 07:46:51 +0100

    portfolio

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,27 @@
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL =
+    "https://my-portfolio-back-end-l6w2.onrender.com/api";
 
-export const apiRequest = async (endpoint, options = {}) => {
-    const response = await fetch(`${BASE_URL}${endpoint}`, {
-        headers: {
-            "Content-Type": "application/json",
-            ...(options.headers || {}),
-        },
-        ...options,
-    });
+export const apiRequest = async (
+    endpoint,
+    options = {}
+) => {
+    const response = await fetch(
+        `${BASE_URL}${endpoint}`,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                ...(options.headers || {}),
+            },
+            ...options,
+        }
+    );
 
     const data = await response.json();
 
     if (!response.ok) {
-        throw new Error(data.message || "Something went wrong");
+        throw new Error(
+            data.message || "Something went wrong"
+        );
     }
 
     return data;
